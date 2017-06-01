e0f531e614dc9eb189656eddc518368f7aebc6a9
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-06-01 13:02:15 -0700

    fixing routes

--- scripts/services/Fixtures.js
+++ scripts/services/Fixtures.js
@@ -1,15 +1,15 @@
 (function () {
     function Fixtures() {
         var Fixtures = {};
-        
+
         var albumCollection = [];
-        
+
         var albumPicasso = {
             title: 'The Colors',
             artist: 'Pablo Picasso',
             label: 'Cubism',
             year: '1881',
-            albumArtUrl: '/assets/images/album_covers/01.png',
+            albumArtUrl: 'assets/images/album_covers/01.png',
             songs: [
                 { title: 'Blue', duration: 161.71, audioUrl: '/assets/music/blue'},
                 { title: 'Green', duration: 103.96, audioUrl: '/assets/music/green' },
@@ -18,13 +18,13 @@
                 { title: 'Magenta', duration: 374.22, audioUrl: '/assets/music/magenta'}
             ]
         };
-        
+
         var albumMarconi = {
              title: 'The Telephone',
              artist: 'Guglielmo Marconi',
              label: 'EM',
              year: '1909',
-             albumArtUrl: '/assets/images/album_covers/20.png',
+             albumArtUrl: 'assets/images/album_covers/20.png',
              songs: [
                  { title: 'Hello, Operator?', duration: '1:01' },
                  { title: 'Ring, ring, ring', duration: '5:01' },
@@ -33,21 +33,21 @@
                  { title: 'Wrong phone number', duration: '2:15'}
              ]
          };
-        
+
         Fixtures.getAlbum = function() {
             return albumPicasso;
         };
-        
+
         Fixtures.getCollection = function(numberOfAlbums) {
             for(var i = 0; i < numberOfAlbums; i++){
                 albumCollection.push(angular.copy(albumPicasso));
             }
             return albumCollection;
         };
-        
+
         return Fixtures;
     }
-    
+
     angular
         .module('blocJams')
         .factory('Fixtures', Fixtures);
